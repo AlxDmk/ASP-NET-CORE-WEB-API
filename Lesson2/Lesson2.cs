c87e439947cd525d4e09ad297fbad174d3e4c8bb
AlxDmk <87035808+AlxDmk@users.noreply.github.com>
2022-04-13 12:34:30 +0300

    ДЗ к Уроку2

---
+++ Lesson2/Lesson2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ASP_NET_CORE_WEB_API.Lesson2
+{
+    public class Lesson2
+    {
+        static void Main(string[] args)
+        {
+            string str = "    Первое   предложние   Второе предложение   Третье   предложение  ";
+            Regex regex = new Regex(@"[ ]{2,}", RegexOptions.None);
+            string result = regex.Replace(str, " ");
+            
+            Regex regex1 = new Regex(@"([ ])([А-Я])");
+            result = regex1.Replace(new StringBuilder(result.Trim()).Append(".").ToString(), ". $2");
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
